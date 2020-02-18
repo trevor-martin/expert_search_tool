79e751c6ac5c4fdcb92bf002e187864386e4ff8d
Trevor Martin <trevormartin1780@gmail.com>
2020-02-18 15:28:17 -0700

    Add basic tests
    Adding basic tests to validate
    object creation.

--- expert_search/models.py
+++ expert_search/models.py
@@ -6,7 +6,6 @@ class Expert(models.Model):
     name = models.CharField(max_length=40)
     personal_website_url = models.CharField(max_length=100)
     shortened_personal_website_url = models.CharField(max_length=100)
-    friendships = ArrayField(models.CharField(max_length=40), null=True)
 
 class Friendships(models.Model):
     expert_id_001 = models.IntegerField()
--- expert_search/tests.py
+++ expert_search/tests.py
@@ -4,16 +4,22 @@ from expert_search.models import Expert, Friendships
 class ExpertTestCase(TestCase):
 
     def setUp(self):
-        pass
+        Expert.objects.create(name='expert001', personal_website_url='website001', shortened_personal_website_url='web001')
+        Expert.objects.create(name='expert002', personal_website_url='website002', shortened_personal_website_url='web002')
 
-    def test001(self):
-        self.assertEquals(1, 1)
+    def test_expert_creation(self):
+        expert = Expert.objects.get(name='expert001')
+        
+        self.assertEquals(expert.personal_website_url, 'website001')
 
 
 class FriendshipsTestCase(TestCase):
 
     def setUp(self):
-        pass
+        Friendships.objects.create(expert_id_001=1, expert_id_002=2)
 
-    def test002(self):
-        self.assertEquals(2, 2)
+    def test_friendship_creation(self):
+        friendship = Friendships.objects.get(expert_id_001=1)
+
+        self.assertEquals(friendship.expert_id_001, 1)
+        self.assertEquals(friendship.expert_id_002, 2)
